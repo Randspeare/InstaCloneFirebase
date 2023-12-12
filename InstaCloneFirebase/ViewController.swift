883ebe7fa2be0cbbd7bfa9b95dd8f788bc044864
NaoTsutomu <iamdez96@gmail.com>
2023-12-12 18:30:17 +0800

    Login/Signup page

--- InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/ViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginClicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    @IBAction func signupClicked(_ sender: UIButton) {
+    }
 }
 
