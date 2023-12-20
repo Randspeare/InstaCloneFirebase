b6e89ddcbac09dc43d371d247d33fa87a08d17d9
eirinjye <95112825+eirinjye@users.noreply.github.com>
2023-12-20 20:16:21 +0800

    Firestore Structure

--- InstaCloneFirebase/UploadViewController.swift
+++ InstaCloneFirebase/UploadViewController.swift
@@ -65,7 +65,15 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
                             
                             
                             //DATABASE
+                            let firestoreDatabase = Firestore.firestore()
+                            var firestoreReference: DocumentReference? = nil
+                            let firestorePost = ["imageURL" : imageURL!, "postedBy" : Auth.auth().currentUser!.email!, "postComment" : self.commentText.text!, "date" : "date", "likes" : 0] as [String: Any]
                             
+                            firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { (error) in
+                                if error != nil {
+                                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                                }
+                            })
                             
                         }
                     }
